5ec5a13aa5b6b4949d4a792421dd8181814f95ae
oroniusbig <oron013@gmail.com>
2025-07-15 15:46:28 +0300

    created userloginhandler <3

---
+++ TriviaUnity/Assets/Scenes/Scripts/UserLoginHandler.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class UserLoginHandler : MonoBehaviour
+{
+    [Header("UI Elements")]
+    public InputField usernameInput;
+    public GameObject RegistrationPanel;
+    public GameObject NameTakenText;
+    public GameObject WaitingPanel;
+
+    [Header("Server URL")]
+    public string serverUrl = "URL"; // Replace with your server URL
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+  
+    
+
+
+}
